67ba2b844569eea99b6d211e0a86eb986211aee6
DanielDG <danielramososielrodrigues@gmail.com>
2024-06-12 17:51:01 -0300

    Update code

---
+++ ContaBanco/README.md
@@ -0,0 +1,10 @@
+# Terminal com input de dados de usuário
+
+## Finalidade
+
+🎯 Programa simples que usa um terminal para armazenar dados em variáveis de um usuário durante a criação de uma conta de banco fictícia
+
+## Packages utilizados
+
+- Scanner
+- Locale
--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -4,20 +4,26 @@ import java.util.Scanner;
 public class ContaTerminal {
 
     public static void main(String[] args) throws Exception {
-    Scanner scanner = new Scanner(System.in). useLocale(Locale.US);
+        // Cria um scanner que possibilita a inserção de dados de forma simples e usa a convenção numérica dos Estados Unidos 
+        Scanner scanner = new Scanner(System.in). useLocale(Locale.US);
 
-    System.out.println("Insira seu numero de Agência:");
-    int numero = scanner.nextInt();
+        // Pede o número da conta/agência do usuário e armazena na variável numero
+        System.out.println("Insira seu numero de Agência:");
+        int numero = scanner.nextInt();
 
-    System.out.println("Insira sua agência:");
-    String agencia = scanner.next();
+        // Pede o código da Agência do usuário e armazena na variável agencia
+        System.out.println("Insira sua agência:");
+        String agencia = scanner.next();
 
-    System.out.println("Insira seu nome:");
-    String nomeCliente = scanner.next();
-    
-    System.out.println("Insira seu saldo:");
-    double saldo = scanner.nextDouble();
+        // Pede o nome do usuário e armazena na variável nomeCliente
+        System.out.println("Insira seu nome:");
+        String nomeCliente = scanner.next();
+        
+        // Pede o saldo inicial do usuário e armazena na variável saldo
+        System.out.println("Insira seu saldo:");
+        double saldo = scanner.nextDouble();
 
-    System.out.println("Olá, " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + numero + " e seu saldo de R$" + saldo + " já está disponível para saque.");
+        // Exibe uma mensagem de sucesso na criação da conta do banco
+        System.out.println("Olá, " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + numero + " e seu saldo de R$" + saldo + " já está disponível para saque.");
     }
 }
