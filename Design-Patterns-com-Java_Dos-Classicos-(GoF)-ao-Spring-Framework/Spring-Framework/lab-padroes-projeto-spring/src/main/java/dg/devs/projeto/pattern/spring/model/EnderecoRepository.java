dd9a8843df73053d04d1ff8cdc4d179ff6a1176e
DanielDG <danielramososielrodrigues@gmail.com>
2024-07-29 22:30:51 -0300

    hotfix
    Signed-off-by: DanielDG <danielramososielrodrigues@gmail.com>

--- Design-Patterns-com-Java_Dos-Classicos-(GoF)-ao-Spring-Framework/Spring-Framework/lab-padroes-projeto-spring/src/main/java/dg/devs/projeto/pattern/spring/model/EnderecoRepository.java
+++
@@ -1,8 +0,0 @@
-package main.java.dg.devs.projeto.pattern.spring.model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class EnderecoRepository {
-}
